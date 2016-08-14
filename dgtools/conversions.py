9a5195fd398639b69d5512d6c43e3a45e5c900f1
Petr Muller <muller@redhat.com>
2016-08-14 18:59:47 +0200

    Add a conversion executable

--- dgtools/conversions.py
+++ dgtools/conversions.py
@@ -1,11 +1,13 @@
 """This module contains classes for converting various input formats to DGTestsuite"""
 
-
+import argparse
+import sys
 import re
 from io import TextIOBase
 
 from dgtools.testsuite import DGTestsuite
 from dgtools.testcase import DGTestCase, DGResult
+
 
 class GitTestsuite(object):
     """Converts a test result file format used by Git project to DGTestsuite"""
@@ -48,3 +50,18 @@ class GitTestsuite(object):
                 testcase.add_check(name, result)
 
         return testsuite
+
+FORMATS = {"git" : GitTestsuite}
+
+def main():
+    """Converts an input stream of a given format to DG testsuite and writes it to the stdout"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("format", choices=("git", ))
+    parser.add_argument("result_file", type=argparse.FileType("r"))
+    parser.add_argument("--name", default="test-result")
+
+    args = parser.parse_args()
+
+    result_parser = FORMATS[args.format]
+    testsuite = result_parser.parse(args.name, args.result_file)
+    testsuite.write_sum_file(sys.stdout)
